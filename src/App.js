96d03af4acdc5b112fb2beeee8a0a469ff51ae31
Tusho <armin.kuhestani@gmail.com>
2018-09-03 14:30:48 +1000

    added test contacts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Reynolds",
+    "email": "ryan@test.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@test.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler Jones",
+    "email": "tyler@test.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
+
 class App extends Component {
   render() {
     return (
