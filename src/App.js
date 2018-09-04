eadf53ecef3d4f2e7880d4b824c7a82361c92601
Tusho <armin.kuhestani@gmail.com>
2018-09-04 16:26:33 +1000

    added button to add contact

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="app">
+      <div className='app'>
       {this.state.screen === 'list' && (
         <ListContacts
           onDeleteContact={this.removeContact}
