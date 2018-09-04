867559699bfd2326cad5b24a3cf19e99631ce510
Tusho <armin.kuhestani@gmail.com>
2018-09-04 14:53:08 +1000

    removed static contacts

--- src/App.js
+++ src/App.js
@@ -3,26 +3,7 @@ import ListContacts from './ListContacts';
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Reynolds",
-        "email": "ryan@test.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@test.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler Jones",
-        "email": "tyler@test.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   }
   removeContact = (contact) => {
     this.setState((state) => ({
