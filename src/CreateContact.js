c38c7a43f1fb11b15a1ca6a8942b880899a14e58
Tusho <armin.kuhestani@gmail.com>
2018-09-04 20:36:05 +1000

    added image input component

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component {
   render() {
     return (
-      <div>Create Contact!</div>
+      <div>
+        <Link className='close-create-contact' to='/'>Close</Link>
+        <form className='create-contact-form'>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+        </form>
+      </div>
     )
   }
 }
