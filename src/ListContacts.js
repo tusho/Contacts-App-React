7ec728cba5ed8f56925bf0b43353d4da67f31d43
Tusho <armin.kuhestani@gmail.com>
2018-09-04 13:05:19 +1000

    added display counter and reset button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -13,8 +13,14 @@ class ListContacts extends Component {
     query: ''
   }
 
-  updateQuery = (query) =>
+  updateQuery = (query) => {
     this.setState({ query: query.trim() })
+  }
+
+
+  clearQuery = (query) => {
+    this.setState({ query: '' })
+  }
 
   render() {
     const { contacts, onDeleteContact } = this.props
@@ -41,6 +47,14 @@ class ListContacts extends Component {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
           {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
